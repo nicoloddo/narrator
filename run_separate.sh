3f1d62c062614c57e35ce14ef2d6904575ee2535
nicoloddo <gnicoloddo@gmail.com>
2024-06-06 21:41:00 +0200

    Add the source venv activation

--- run_separate.sh
+++ run_separate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Terminal 1: Runs capture.py
-lxterminal --command="bash -c '. setenv.sh; . setvoice.sh; python3 capture.py; exec bash'" &
+lxterminal --command="bash -c 'source venv/bin/activate; . setenv.sh; . setvoice.sh; python3 capture.py; exec bash'" &
 
 # Terminal 2: Runs narrator.py
-lxterminal --command="bash -c '. setenv.sh; . setvoice.sh; python3 narrator.py; exec bash'" &
+lxterminal --command="bash -c 'source venv/bin/activate; . setenv.sh; . setvoice.sh; python3 narrator.py; exec bash'" &
 
 echo "Both scripts are now running in separate terminals."
