1ce510afaa3308a727b6f18f4a9a769585ea137a
nicoloddo <gnicoloddo@gmail.com>
2024-06-11 20:16:26 +0200

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -3,4 +3,4 @@ export AGENT_PROMPT="You are Sir David Attenborough. Narrate the picture of the 
 export FIRST_IMAGE_PROMPT="You are starting the documentary. This is the first thing to show."
 export NEW_IMAGE_PROMPT="Continue the documentary by talking about this image. You are Sir David Attenborough"
 export ELEVENLABS_VOICE_ID=Uql9NfKStxPGW1kwk20T
-export PLAYHT_VOICE_ID=s3://voice-cloning-zero-shot/e8751c5e-c28e-4811-8b4f-992dfad28887/original/manifest.json
+export PLAYHT_VOICE_ID="s3://voice-cloning-zero-shot/e8751c5e-c28e-4811-8b4f-992dfad28887/original/manifest.json"
