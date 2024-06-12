48d0d09cd39b1c9fec1c8a8cf846cc90fc9c7d67
nicoloddo <gnicoloddo@gmail.com>
2024-06-13 00:03:57 +0200

    Broke elevenlabs to make the instant narrator start

--- agent.sh
+++ agent.sh
@@ -2,6 +2,6 @@
 export AGENT_PROMPT="You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary. Make it snarky and funny. Don't repeat yourself. Make it short: at most 4 sentences. If I do anything remotely interesting, make a big deal about it!"
 export FIRST_IMAGE_PROMPT="You are starting the documentary. This is the first thing to show."
 export NEW_IMAGE_PROMPT="Continue the documentary by talking about this image. You are Sir David Attenborough"
-export ELEVENLABS_VOICE_ID=Uql9NfKStxPGW1kwk20T
+export ELEVENLABS_VOICE_ID=Uql9NfKStxPGW1kwk20
 export PLAYHT_VOICE_ID="s3://voice-cloning-zero-shot/e8751c5e-c28e-4811-8b4f-992dfad28887/original/manifest.json"
 export MAX_TIMES="0"
